ca72a0f678b6b8aaa7f5f78cc5505f8e3be7f0dc
NasifQadri <NasifQadri@users.noreply.github.com>
2020-02-08 17:46:54 -0500

    Add json dumps
    Currently unused

---
+++ dump.py
@@ -0,0 +1,13 @@
+## DUMP DATA FROMPCPARTPICKER API INTO JSON FILE
+
+from pcpartpicker import API
+from json import loads
+api = API()
+
+parts = ["cpu","memory","video-card"]
+
+for i in parts:
+    j = api.retrieve(i)
+    j = j.to_json()
+    with open(i + ".json", "w+") as file:
+        file.write((j))
