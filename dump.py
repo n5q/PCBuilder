146798fcac8da0d63ac818b2b2ac7e66ed857a39
NasifQadri <NasifQadri@users.noreply.github.com>
2020-02-09 09:45:56 -0500

    Update files

--- dump.py
+++ dump.py
@@ -4,14 +4,10 @@ from pcpartpicker import API
 from json import loads
 api = API()
 
-# parts = ["cpu","memory","video-card"]
+parts = []
 
-# for i in parts:
-#     j = api.retrieve(i)
-#     j = j.to_json()
-#     with open(i + ".json", "w+") as file:
-#         file.write((j))
-j = api.retrieve_all()
-j = j.to_json()
-with open("all.json", "w+") as file:
-    file.write((j))
+for i in parts:
+    j = api.retrieve(i)
+    j = j.to_json()
+    with open(i + ".json", "w+") as file:
+        file.write((j))
